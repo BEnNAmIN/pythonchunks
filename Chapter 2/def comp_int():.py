75367bd379be2e53a75ac7d145b2f349edcb6804
BEnNAmIN <75086586+BEnNAmIN@users.noreply.github.com>
2023-09-13 22:06:05 -0700

    Delete Chapter 2/def comp_int():.py

--- Chapter 2/def comp_int():.py
+++
@@ -1,9 +0,0 @@
-def comp_int():
-    P = int(input("How much money starts in the account: "))
-    r = float(input("what is the interest rate: "))
-    r /= 100
-    n = int(input("How many times is the interest paid per year: "))
-    t = int(input("Specify a time frame in years: "))
-    A = P*(1 + (r/n))**(n*t)
-    print("After ",t, "years, you will have a total of $",A)
-comp_int()
