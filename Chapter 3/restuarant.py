e90eb513b38ac551080133ae6601d4984aec7cde
Ben <tobipower.amazing@gmail.com>
2023-09-21 10:55:45 -0700

    m

--- Chapter 3/restuarant.py
+++ Chapter 3/restuarant.py
@@ -21,4 +21,20 @@ if vege == "yes" or "Yes":
         print("Main Street Pizza Company")
         print("Mama's Fine Italian Kitchen")
 else:
-    
+    if gl_fr == "yes" or "Yes":
+        if vega == "yes" or "Yes":
+            print("Corner Cafe")
+            print("The Chef's Kitchen")
+        else:
+            print("Corner Cafe")
+            print("The Chef's Kitchen")
+            print("Main Street Pizza Company")
+    elif vega == "yes" or "Yes":
+        print("The Chef's Kitchen")
+        print("Corner Cafe")
+    else:
+        print("The Chef's Kitchen")
+        print("Corner Cafe")
+        print("Main Street Pizza Company")
+        print("Mama's Fine Italian Kitchen")
+        print("Joe's Gourmet Burgers")
